332ec15c896afacfaa6597495ea5d754f915b444
leo marache <leo.marache@etu.univ-cotedazur.fr>
2020-03-12 11:13:53 +0100

    ajout sous dependance

---
+++ src/main/java/fr/unice/polytech/si3/qgl/theblackpearl/shape/Point.java
@@ -0,0 +1,18 @@
+package fr.unice.polytech.si3.qgl.theblackpearl.shape;
+
+public class Point {
+    private double x,y;
+
+    public Point(double a, double b){
+        this.x = a;
+        this.y = b;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
